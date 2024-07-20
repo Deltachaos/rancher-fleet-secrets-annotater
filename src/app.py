c0fdd52eb2966b515ea3af8edbd2654271d83638
Maximilian Ruta <mruta@xtain.net>
2024-07-20 15:58:58 +0200

    first commit

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import json
+import base64
+import yaml
+from re import L
+import kubernetes
+from io import StringIO
+import os
+import re
+import time
+
+while True:
+  print("not implemented yet")
+  time.sleep(5)
